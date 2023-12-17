896151916ce31e81c8b393dfb004b9e7447f2d95
ALLAL Gwénolé <gwenole.a@gmail.com>
2023-12-17 18:04:51 +0100

    - Ajout prototype Menu Pause\n-Test Retour à la ligne commit\n-J'espère ça fonctionne

---
+++ InterfaceUtilisateur/pausemenu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@export var pause_manager : PauseManager
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	pause_manager.connect("toggle_game_paused", _on_scene_toggle_game_pause)
+
+
+func _on_scene_toggle_game_pause(is_paused : bool):
+	if (is_paused):
+		show()
+	else :
+		hide()
+
+
+func _on_continuer_pressed():
+	pause_manager.game_paused = false
+
+
+func _on_quitter_pressed():
+	get_tree().quit(0);
+
+
+func _on_recommencer_pressed():
+	get_tree().change_scene_to_file("res://Scène/Amboise.tscn")
+	get_tree().reload_current_scene()
